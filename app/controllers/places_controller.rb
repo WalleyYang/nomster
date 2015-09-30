2daa999f53df2e49f7ff78bb189e36c633d12b3f
WalleyWorks <walleyyang@gmail.com>
2015-09-30 23:06:42 +0000

    Added submit button to place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,6 +7,19 @@ class PlacesController < ApplicationController
 	def new
 	  @place = Place.new
 	end
+
+	# Sends to database
+    def create
+      Place.create(place_params)
+      redirect_to root_path
+    end
+
+    private
+
+    # Gets values from place form
+    def place_params
+      params.require(:place).permit(:name, :description, :address)
+    end
 end
 
 
