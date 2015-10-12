45484e8b7a38c63e04f7989b7d02ce472c23b886
walleyworks <walleyyang@gmail.com>
2015-10-12 20:22:25 +0000

    Destroy places only by creators

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
   	# Before filter to require user to be logged in for new and create
-    before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
+    before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
     def index
   	  # Implemented Kaminari paginate
@@ -42,6 +42,11 @@ class PlacesController < ApplicationController
 
     def destroy
       @place = Place.find(params[:id])
+      
+      if @place.user != current_user
+        return render :text => 'Not Allowed', :status => :forbidden
+      end
+      
       @place.destroy
       redirect_to root_path
     end
