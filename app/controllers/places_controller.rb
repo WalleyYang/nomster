24e6057a0f596b337dbdc0a456b1f50851b31a40
walleyworks <walleyyang@gmail.com>
2015-10-12 16:52:13 +0000

    Added page to show more details for single place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,20 +1,24 @@
 class PlacesController < ApplicationController
-	# Before filter to require user to be logged in for new and create
-  before_action :authenticate_user!, :only => [:new, :create]
+  	# Before filter to require user to be logged in for new and create
+    before_action :authenticate_user!, :only => [:new, :create]
 
-  def index
-	  # Implemented Kaminari paginate
-	  @places = Place.order(:name).page params[:page]
-	end
+    def index
+  	  # Implemented Kaminari paginate
+  	  @places = Place.order(:name).page params[:page]
+  	end
 
-	def new
-	  @place = Place.new
-	end
+  	def new
+  	  @place = Place.new
+  	end
 
 	# Sends to database
     def create
       current_user.places.create(place_params)
       redirect_to root_path
+    end
+
+    def show
+      @place = Place.find(params[:id])
     end
 
     private
