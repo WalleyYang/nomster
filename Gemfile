8ddc526894aa569d323315eff2842daa2290f4f1
walleyworks <walleyyang@gmail.com>
2015-10-12 23:12:28 +0000

    Added Bing Map API

--- Gemfile
+++ Gemfile
@@ -45,6 +45,12 @@ gem 'simple_form'
 # Devise gem authentication
 gem 'devise', '>= 3.2.4'
 
+# Geocoder
+gem 'geocoder'
+
+# Figaro
+gem "figaro", ">= 1.0.0"
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- app/models/place.rb
+++ app/models/place.rb
@@ -5,6 +5,10 @@ class Place < ActiveRecord::Base
   # Shortcut to give us places associated with user
   belongs_to :user
 
+  # Geocoder
+  geocoded_by :address
+  after_validation :geocode
+
   # Enter name for place
   validates :address, :description, :presence => true 
   validates :name, :presence => true, length: { minimum: 3 }
