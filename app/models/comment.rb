9b017dcb35b44ff6d328b07ef6dd76d634ee9c99
Walley Yang <walleyyang@gmail.com>
2015-10-20 20:05:22 +0000

    Added triggering email when comments added

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,17 +1,23 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :place
+	after_create :send_comment_email
 
-  	RATINGS = {
-	    'one star'    => '1_star',
-	    'two stars'   => '2_stars',
-	    'three stars' => '3_stars',
-	    'four stars'  => '4_stars',
-	    'five stars'  => '5_stars'
-  	}
+	RATINGS = {
+		'one star'    => '1_star',
+		'two stars'   => '2_stars',
+		'three stars' => '3_stars',
+		'four stars'  => '4_stars',
+		'five stars'  => '5_stars'
+	}
 
   	# Invert rating table and give readble text
   	def humanized_rating
-    	RATINGS.invert[self.rating]
+  		RATINGS.invert[self.rating]
   	end
-end
+
+  	def send_comment_email
+  		NotificationMailer.comment_added(self).deliver
+  	end
+
+  end
