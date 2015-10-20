bb0d702e45fa304d943303a63d2fcc907e9eb23d
Walley Yang <walleyyang@gmail.com>
2015-10-20 20:59:58 +0000

    Heroku fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Nomster::Application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
+    :domain         => 'nomster-walley.heroku.com',
     :authentication => :plain,
   }
   
